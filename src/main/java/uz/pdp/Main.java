4a9a56e545c379cd1d1d3bdf815e47430d07a905
lazik17 <mywave.17@gamail.com>
2024-10-03 14:02:33 +0500

    commit

--- src/main/java/uz/pdp/Main.java
+++ src/main/java/uz/pdp/Main.java
@@ -1,5 +1,7 @@
 package uz.pdp;
 
+import uz.pdp.medium.CheckIfArrayPairsAreDivisibleByK;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
