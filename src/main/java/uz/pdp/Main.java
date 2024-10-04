739d7ddefa1063473c2bb31ad2deb3e0dc4729c2
lazik17 <mywave.17@gamail.com>
2024-10-04 10:34:14 +0500

    commit4

--- src/main/java/uz/pdp/Main.java
+++ src/main/java/uz/pdp/Main.java
@@ -2,8 +2,14 @@ package uz.pdp;
 
 import java.util.Arrays;
 import uz.pdp.easy.TwoSum;
-import uz.pdp.medium.CheckIfArrayPairsAreDivisibleByK;
+//1. Two Sum
+//       Easy
 
+//Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+//
+//You may assume that each input would have exactly one solution, and you may not use the same element twice.
+//
+//You can return the answer in any order.
 public class Main {
     public static void main(String[] args) {
 //        CheckIfArrayPairsAreDivisibleByK check = new CheckIfArrayPairsAreDivisibleByK();
