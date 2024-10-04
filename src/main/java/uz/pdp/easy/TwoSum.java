b39a634b8d1cca7f5f1eb0bc2c7fe2f63352c081
lazik17 <mywave.17@gamail.com>
2024-10-04 17:14:15 +0500

    2 medium

--- src/main/java/uz/pdp/easy/TwoSum.java
+++ src/main/java/uz/pdp/easy/TwoSum.java
@@ -1,5 +1,12 @@
 package uz.pdp.easy;
+//1. Two Sum
+//       Easy
 
+//Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+//
+//You may assume that each input would have exactly one solution, and you may not use the same element twice.
+//
+//You can return the answer in any order.
 public class TwoSum {
     public int[] twoSum(int[] nums, int target) {
         int[] res = new int[2];
