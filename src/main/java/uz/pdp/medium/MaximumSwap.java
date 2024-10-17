86e1434b6b967169e953604f16b102a529522319
lazik17 <mywave.17@gamail.com>
2024-10-17 21:09:09 +0500

    670-medium

---
+++ src/main/java/uz/pdp/medium/MaximumSwap.java
@@ -0,0 +1,32 @@
+package uz.pdp.medium;
+//670. Maximum Swap
+//Medium
+//You are given an integer num. You can swap two digits at most once to get the maximum valued number.
+//
+//Return the maximum valued number you can get.
+//
+
+public class MaximumSwap {
+    public int maximumSwap(int num) {
+        char[] numArr = Integer.toString(num).toCharArray();
+        int n = numArr.length;
+
+        int[] last = new int[10];
+        for (int i = 0; i < n; i++) {
+            last[numArr[i] - '0'] = i;
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int d = 9; d > numArr[i] - '0'; d--) {
+                if (last[d] > i) {
+                    char temp = numArr[i];
+                    numArr[i] = numArr[last[d]];
+                    numArr[last[d]] = temp;
+                    return Integer.parseInt(new String(numArr));
+                }
+            }
+        }
+
+        return num;
+    }
+}
