bb8af281d9bdbcb2459397da8a4cf38aa22ae6fa
Lazizbek <mywave.17@gmail.com>
2024-11-20 23:35:12 +0100

    some commit

--- src/main/java/uz/pdp/medium/TakeKOfEachCharacterFromLeftAndRight.java
+++ src/main/java/uz/pdp/medium/TakeKOfEachCharacterFromLeftAndRight.java
@@ -16,7 +16,7 @@ public class TakeKOfEachCharacterFromLeftAndRight {
         int cur, len = c.length;
         for (cur = 0; cur < len; cur++) {
             arr[c[cur] - 'a']++;
-            if (arr[0] >= k && arr[1] >= k && arr[2] >= k) break;
+            if (arr[0] >= k &&   arr[1] >= k && arr[2] >= k) break;
         }
         if (cur == len) return -1;
         int count = cur + 1, min = count, end = len - 1;
